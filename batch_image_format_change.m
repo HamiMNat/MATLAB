3e68d58c0e48817bb30b569e16fd0fae854b62e7
Hami <mahdavinataj.hami@gmail.com>
2018-11-17 13:50:43 +0330

    Update batch_image_format_change.m

--- batch_image_format_change.m
+++ batch_image_format_change.m
@@ -10,7 +10,7 @@ dst_format = 'png'; % destination format
 src_list = dir([src_dir,'*.',scr_format]);
 %strrep()
 for i = 1:length(src_list)
-    img = imread(src_list(i).name);
+    img = imread([src_dir src_list(i).name]);
     imwrite(img,[dst_dir,src_list(i).name(1:end-3),dst_format])
-    delete(src_list(i).name); % uncomment to delete the source files
+    delete([src_dir src_list(i).name]); % uncomment to delete the source files
 end
